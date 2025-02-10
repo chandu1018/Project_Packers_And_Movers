b4fe119282888b9ccd4191bb53fe12029203f230
rushic86635 <rushi.sunbeam2024@gmail.com>
2025-02-10 23:42:37 +0530

    User Registration and Login with headers

---
+++ packers-movers/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+import UserRegisterForm from "../components/UserRegisterForm";
+import Footer from "../components/Footer";
+
+import "../styles/form.css";
+import Headerh from "../components/Headerh";
+
+export default function RegisterPage() {
+  return (
+    <div className="register_page">
+      <Headerh />
+      <UserRegisterForm />
+      <Footer />
+    </div>
+  );
+}
